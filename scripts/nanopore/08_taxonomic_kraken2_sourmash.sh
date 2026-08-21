0628611e5478963d7cba974ddb42841463591bc3
Stefanel99 <145434847+Stefanel99@users.noreply.github.com>
2026-08-21 17:18:50 +0200

    Enhance script to clean up files and generate outputs
    Added steps to remove signature and CSV files after processing, and create KRONA and Kreport outputs.

--- scripts/nanopore/08_taxonomic_kraken2_sourmash.sh
+++ scripts/nanopore/08_taxonomic_kraken2_sourmash.sh
@@ -30,4 +30,22 @@ echo "Creating a CSV file..."
 sourmash gather -k 31 "/proj/rumen_interaction/NOBACKUP/results/Nanopore/k2_sourmash/${f_name}.sig" /proj/rumen_interaction/NOBACKUP/genomes/GTDB/gtdb-rs207.genomic.k31.zip -o "/proj/rumen_interaction/NOBACKUP/results/Nanopore/k2_sourmash/${f_name}.gather.k31.csv"
 echo "CSV file created !"
 
+echo "Removing the signature file ..."
+rm "/proj/rumen_interaction/NOBACKUP/results/Nanopore/k2_sourmash/${f_name}.sig"
+echo "Signature file removed !"
+
+echo "Creating a KRONA output..."
+sourmash tax metagenome --gather -csv "/proj/rumen_interaction/NOBACKUP/results/Nanopore/k2_sourmash/${f_name}.gather.k31.csv" --taxonomy /proj/rumen_interaction/NOBACKUP/genomes/GTDB/gtdb-rs207.taxonomy.with-strain.csv.gz --output -format krona --rank species > "/proj/rumen_interaction/NOBACKUP/results/Nanopore/k2_sourmash/${f_name}.krona"
+echo "KRONA output created !"
+
+echo "Creating a Kreport output..."
+sourmash tax metagenome --gather -csv "/proj/rumen_interaction/NOBACKUP/results/Nanopore/k2_sourmash/${f_name}.gather.k31.csv" --taxonomy /proj/rumen_interaction/NOBACKUP/genomes/GTDB/gtdb-rs207.taxonomy.with-strain.csv.gz --output -format kreport > "/proj/rumen_interaction/NOBACKUP/results/Nanopore/k2_sourmash/${f_name}.kreport"
+echo "Kreport output created !"
+
+echo "Removing the CSV file..."
+rm "/proj/rumen_interaction/NOBACKUP/results/Nanopore/k2_sourmash/${f_name}.gather.k31.csv"
+echo "CSV file removed !"
+done
+
+
 
