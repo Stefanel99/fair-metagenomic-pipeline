eaf658c31520544364bec0e054baf2342083c5a8
Stefanel99 <145434847+Stefanel99@users.noreply.github.com>
2026-08-21 17:49:15 +0200

    Add quality control script for unmapped Illumina reads
    This script processes gzipped alignment files, counts the number of reads, and runs FastQC and MultiQC for quality control analysis.

---
+++ scripts/illumina/03_quality_control_unmapped.sh
@@ -0,0 +1,20 @@
+for file in $(ls /proj/rumen_interaction/NOBACKUP/results/Illumina/bowtie2/alignement/*.gz)
+do
+file_name=$(echo $file | cut -d’/’ -f9 | cut -d’.’ -f1)
+echo $(zcat $file | grep -c ’^@’) : $file_name >> /proj/rumen_interaction/NOBACKUP/results/Illumina/counting/count_qc_after_map.txt
+done
+
+
+echo "Loading fastqc..."
+module load conda
+module load fastqc
+./opt/sw/conda/3/etc/profile.d/conda.sh
+echo "Fastqc loaded !"
+
+fastqc -t 20 /proj/rumen_interaction/NOBACKUP/results/Illumina/bowtie2/alignement/*.gz -o /proj/rumen_interaction/NOBACKUP/results/Illumina/full_analysis/fastqc_unmap_rep/
+
+echo "Loading multiqc..."
+module load multiqc
+echo "Multiqc loaded !"
+
+multiqc /proj/rumen_interaction/NOBACKUP/results/Illumina/full_analysis/fastqc_unmap_rep/ -o /proj/rumen_interaction/NOBACKUP/results/Illumina/full_analysis/fastqc_unmap_rep/multiqc/
